50f04bbe44a0d380be0451c56e8a8f571c0cd359
taktak-0319 <tka150319@icloud.com>
2023-03-18 17:24:41 +0900

    ボタンの作成

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,5 @@
+import Item_button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Button" };
+export const Button = () => Item_button;
